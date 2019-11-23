8a36969382ce3dfdde05bd2a59494c3d74be2059
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-11-22 21:20:00 -0500

    first population

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = function theme () {
-    
+module.exports = {
+    heroGroup: [
+        "Superman",
+        "Batman",
+        "Aquaman",
+        "Cyborg",
+        "Spider-Man"
+    ],
+
+    villainGroup: [
+        "Magneto",
+        "Bane",
+        "Thanos",
+        "Doomsday"
+    ],
+
+    hero: function (){
+        return heroGroup[Math.floor(Math.random()*heroGroup.length)];
+    },
+
+    villain: function (){
+        return villainGroup[Math.floor(Math.random()*villainGroup.length)];
+    }
+
 }
